5aad36ae019f987a6ae095f639b16e88669bbdd9
usuario01zeroum <seu-email@exemplo.com>
2025-10-18 20:08:57 -0300

    commitAtiv6

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -2,7 +2,7 @@ namespace Exo.WebApi
 {
     public class Usuario
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
     }
